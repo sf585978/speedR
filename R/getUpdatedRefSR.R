569a34a89dd0ebc1952b42f8c7f0a225a0f04ca6
Sean Fischer <sean.fischer@asc.upenn.edu>
2018-11-04 22:25:32 -0500

    updated function
    updated the getUpdatedRefSR() function so that it is more efficient. The efficiency comes from only updating runners in a specific race and then combining them with those runners not updated. Also, most recent year is recorded.

--- R/getUpdatedRefSR.R
+++ R/getUpdatedRefSR.R
@@ -1,7 +1,8 @@
 #' Get Updating Reference Speed Rating
 #'
-#' This function inputs a master file of speed ratings for a year and a list of races and returns a list of updated reference speed ratings for the runners contained in the master list.
+#' This function inputs a master file of speed ratings and references and returns a list of updated reference speed ratings for the runners contained in the master list.
 #' @param allSpeedRatings The master speed rating data frame
+#' @param updatedReference The master updated reference data frame
 #' @param race The raceID for the newest race
 #' @return updatedReferences The data frame of updated reference speed ratings
 #' @keywords speed rating, cross country, handicapping
@@ -9,17 +10,20 @@
 #' @examples
 #' getUpdatedRefSR(allSpeedRatings, c("mOswego17", "mWilliams17", "mGeneseo17"))
 
-getUpdatedRefSR <- function(allSpeedRatings, race) {
+getUpdatedRefSR <- function(allSpeedRatings, updatedReference, race) {
   require(dplyr)
-  latestResults <- allSpeedRatings %>%
+  require(progress)
+  uniques <- allSpeedRatings %>%
     filter(Race == race)
-  uniques <- unique(latestResults[c("Name", "School")])
+  uniques <- unique(uniques[c("Name", "School")])
   counts <- numeric(length(uniques$Name))
   out <- numeric(length(uniques$Name))
+  years <- rep(year, length(uniques$Name))
+  pb <- progress_bar$new(total = length(uniques$Name))
   for (i in 1:length(uniques$Name)) {
     individualResults <- allSpeedRatings %>%
-      filter(Name == uniques$Name[i],
-             School == uniques$School[i])
+      filter(Name == uniques$Name[i]) %>%
+      filter(School == uniques$School[i])
     nResults <- length(individualResults$`Speed Rating`)
     w <- rep(1, nResults)
     # w[which(individualResults$`Speed Rating` ==
@@ -43,8 +47,18 @@ getUpdatedRefSR <- function(allSpeedRatings, race) {
     w[nResults] <- w[nResults] + 0.5
     out[i] <- weighted.mean(individualResults$`Speed Rating`, w = w)
     counts[i] <- nResults
+    years[i] <- max(individualResults$Year)
+    pb$tick()
   }
-  updatedReferences <- as.data.frame(cbind(uniques, out, counts))
-  colnames(updatedReferences) <- c("name", "school", "refSR", "Number of Races")
+  updatedReferences <- as.data.frame(cbind(uniques, out, counts, years))
+  colnames(updatedReferences) <- c("name", "school", "refSR", "Number of Races",
+                                   "Most Recent Year")
+
+  notUpdatedReferences <- updatedReference %>%
+    anti_join(updatedReferences, by = c("name" = "name", "school" = "school"))
+
+  updatedReferences <- as.data.frame(rbind(updatedReferences,
+                                           notUpdatedReferences))
+
   return(updatedReferences)
 }
