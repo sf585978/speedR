d9f5ce2c88a1bcdabd4b44b541b4f68b413a6252
Sean Fischer <sean.fischer@asc.upenn.edu>
2018-11-04 21:34:30 -0500

    updated function
    updated the getUpdatedRefSR() function so that it only tries to update the reference value for runners in a specific race. No idea if this will break the function.

--- R/getUpdatedRefSR.R
+++ R/getUpdatedRefSR.R
@@ -2,21 +2,24 @@
 #'
 #' This function inputs a master file of speed ratings for a year and a list of races and returns a list of updated reference speed ratings for the runners contained in the master list.
 #' @param allSpeedRatings The master speed rating data frame
-#' @param races The list of races in chronological order
+#' @param race The raceID for the newest race
 #' @return updatedReferences The data frame of updated reference speed ratings
 #' @keywords speed rating, cross country, handicapping
 #' @export
 #' @examples
 #' getUpdatedRefSR(allSpeedRatings, c("mOswego17", "mWilliams17", "mGeneseo17"))
 
-getUpdatedRefSR <- function(allSpeedRatings, races) {
-  uniques <- unique(allSpeedRatings[c("Name", "School")])
+getUpdatedRefSR <- function(allSpeedRatings, race) {
+  require(dplyr)
+  latestResults <- allSpeedRatings %>%
+    filter(Race == race)
+  uniques <- unique(latestResults[c("Name", "School")])
   counts <- numeric(length(uniques$Name))
   out <- numeric(length(uniques$Name))
   for (i in 1:length(uniques$Name)) {
     individualResults <- allSpeedRatings %>%
-      filter(Name == uniques$Name[i]) %>%
-      filter(School == uniques$School[i])
+      filter(Name == uniques$Name[i],
+             School == uniques$School[i])
     nResults <- length(individualResults$`Speed Rating`)
     w <- rep(1, nResults)
     # w[which(individualResults$`Speed Rating` ==
@@ -39,7 +42,7 @@ getUpdatedRefSR <- function(allSpeedRatings, races) {
     }
     w[nResults] <- w[nResults] + 0.5
     out[i] <- weighted.mean(individualResults$`Speed Rating`, w = w)
-    counts[i] <- length(individualResults$`Speed Rating`)
+    counts[i] <- nResults
   }
   updatedReferences <- as.data.frame(cbind(uniques, out, counts))
   colnames(updatedReferences) <- c("name", "school", "refSR", "Number of Races")
